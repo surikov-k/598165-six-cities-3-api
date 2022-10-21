db60f06c16889201e03fd36de4a33571e144695c
Konstantin Surikov <surikov@gmail.com>
2022-10-21 15:03:52 +0300

    Refactored getting a list of offers

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -5,13 +5,13 @@ import { UserEntity } from './user.entity.js';
 import { inject, injectable } from 'inversify';
 import { Component } from '../../types/component.types.js';
 import { LoggerInterface } from '../../common/logger/logger.interface.js';
+import LoginUserDto from './dto/login-user.dto.js';
 
 @injectable()
 export default class UserService implements UserServiceInterface {
   constructor(
     @inject(Component.LoggerInterface)
     private logger: LoggerInterface,
-
     @inject(Component.UserModel)
     private readonly userModel: types.ModelType<UserEntity>
   ) {}
@@ -28,7 +28,7 @@ export default class UserService implements UserServiceInterface {
   }
 
   public async findByEmail(email: string):
-    Promise<DocumentType<UserEntity>  | null> {
+    Promise<DocumentType<UserEntity> | null> {
     return this.userModel.findOne({email});
   }
 
@@ -45,5 +45,19 @@ export default class UserService implements UserServiceInterface {
     }
 
     return this.create(dto, salt);
+  }
+
+  public async verifyUser(dto: LoginUserDto, salt: string): Promise<DocumentType<UserEntity> | null> {
+    const user = await this.findByEmail(dto.email);
+
+    if (!user) {
+      return null;
+    }
+
+    if (user.verifyPassword(dto.password, salt)) {
+      return user;
+    }
+
+    return null;
   }
 }
